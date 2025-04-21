c043902d5e7c403973c785a2e19b8f22381020aa
Shehanshah1 <MuhammadAsjad.RehmanHashmi@gmail.com>
2025-04-21 00:44:20 -0500

    t

--- app/components/NameTransition.tsx
+++ app/components/NameTransition.tsx
@@ -7,23 +7,23 @@ export function NameTransition() {
   return (
     <ViewTransition>
       <Link href="/about">
-        <h1 className="font-medium leading-[1.5em] pt-12 text-4xl sm:text-2xl md:text-4xl hover:text-blue-500 transition-colors duration-300 text-center cursor-pointer">
+        <h1 className="font-medium  leading-[1.5em] pt-12 text-3xl sm:text-4xl md:text-5xl hover:text-blue-500 transition-colors duration-300 text-center cursor-pointer">
           <span className="sr-only">Asjad Rehman</span>
           <span
             aria-hidden="true"
             className="block overflow-hidden group relative"
           >
             {/* English default */}
-            <span className="inline-block transition-all sm:text-1xl md:text-5xl duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
-            M. Asjad Rehman Hashmi
+            <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full whitespace-nowrap">
+              M. Asjad Rehman Hashmi
             </span>
 
             {/* Arabic hover */}
             <span
               dir="rtl"
-              className="inline-block absolute left-0 top-0 sm:text-1xl md:text-5xl transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0 whitespace-nowrap"
+              className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0 whitespace-nowrap"
             >
- محمد أسجد رحمان هاشمی     
+              محمد أسجد رحمان هاشمی
             </span>
           </span>
         </h1>
