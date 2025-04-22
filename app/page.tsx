2d68cd6cd7d1a8feb567f9ec637aa67b8c2da5ab
Shehanshah1 <MuhammadAsjad.RehmanHashmi@gmail.com>
2025-04-21 21:08:32 -0500

    second blog

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,30 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <section className="max-w-2xl mx-auto px-4 py-10 text-justify leading-relaxed">
+    <section className="max-w-3xl mx-auto px-4 py-10 text-justify leading-relaxed">
       <NameTransition />
-    
+
       <p className="mt-6 text-lg">
         I’m a student of political science and computer science with a growing interest in revolutionary theory, political Islam, and conflict studies. My work spans from building software to exploring ideas around state power, resistance, and identity — including emerging areas like cyber ethics.
       </p>
 
       <h2 className="text-xl font-semibold mt-10">📝 Writing</h2>
-      <ul className="list-disc pl-6 space-y-2 mt-2">
+      <ul className="list-disc pl-6 space-y-1 mt-2">
         <li>
           <Link href="/blog/failure-of-liberal-world-order" className="text-blue-500 hover:underline">
             Failure of the Liberal World Order
           </Link>
         </li>
+        <li>
+          <Link href="/blog/art-and-dissent" className="text-blue-500 hover:underline">
+          Art and Dissent: How Art Reflects American Politics and Decisions
+          </Link>
+
+        </li>
       </ul>
 
       <h2 className="text-xl font-semibold mt-10">💻 Code Projects</h2>
-      <ul className="list-disc pl-6 space-y-2 mt-2">
+      <ul className="list-disc pl-6 space-y-1 mt-2">
         <li>
           <a href="https://github.com/Shehanshah1/MadinaEnterprises" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">
             Madina Enterprises Full-Stack System
