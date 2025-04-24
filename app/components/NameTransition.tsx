e87bd4941c3a70d71bcfe958fc71aa02fd3695d3
Shehanshah1 <MuhammadAsjad.RehmanHashmi@gmail.com>
2025-04-23 23:46:55 -0500

    fix

--- app/components/NameTransition.tsx
+++ app/components/NameTransition.tsx
@@ -7,22 +7,21 @@ export function NameTransition() {
   return (
     <ViewTransition>
       <Link href="/about">
-      <h1 className="font-medium pt-12 text-2xl sm:text-3xl md:text-4xl leading-tight hover:text-blue-500 transition-colors duration-300 text-left cursor-pointer break-words">
-          <span className="sr-only">Asjad Rehman</span>
+      <h1 className="font-medium pt-12 text-2xl sm:text-3xl md:text-5xl lg:text-6xl leading-tight text-center sm:text-left break-words max-w-full">
+      <span className="sr-only">Asjad Rehman</span>
           <span
             aria-hidden="true"
             className="block overflow-hidden group relative"
           >
             {/* English default */}
-            <span className="inline-block transition-all leading-tight text-xl sm:text-2xl md:text-5xl duration-300 ease-in-out group-hover:-translate-y-full break-words">
-              M. Asjad Rehman Hashmi
+            <span className="inline-block transition-all text-2xl sm:text-3xl md:text-5xl lg:text-6xl leading-tight duration-300 ease-in-out group-hover:-translate-y-full break-words text-center sm:text-left">
+            M. Asjad Rehman Hashmi
             </span>
 
             <span
   dir="rtl"
-  className="inline-block absolute left-0 top-0 leading-tight text-xl sm:text-2xl md:text-5xl transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0 break-words"
+  className="inline-block absolute left-0 top-0 transition-all text-2xl sm:text-3xl md:text-5xl lg:text-6xl leading-tight duration-300 ease-in-out translate-y-full group-hover:translate-y-0 break-words text-center sm:text-left"
 >
-
               محمد أسجد رحمان ہاشمی
             </span>
           </span>
